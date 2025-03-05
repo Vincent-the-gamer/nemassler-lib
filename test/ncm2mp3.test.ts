fd30b90e8956f01728dc38c4b3802adb52be0c7b
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-03-05 14:17:10 +0800

    chore: release v0.1.0-alpha.

--- test/ncm2mp3.test.ts
+++ test/ncm2mp3.test.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error .node file
-import { ncm2mp3 } from "../index.node"
+import { ncm2mp3 } from "../lib"
 
 const result = ncm2mp3("/Users/guifeng/Downloads/ncm","/Users/guifeng/Downloads/mp3")
 
